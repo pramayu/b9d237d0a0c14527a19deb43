55a1cbcc6d4deb463554f21392e50bed4962aff5
pramayu <gedepramayu@gmail.com>
2020-01-19 22:57:25 +0800

    classroom3

--- app/components/classroom/history.js
+++ app/components/classroom/history.js
@@ -13,10 +13,10 @@ const History = (props) => {
         <View style={{width: '100%', height: '100%'}}>
             <View style={{width: '100%', flexDirection: 'row'}}>
                 <View style={{width: '8%'}}>
-                    <SimpleLineIcons name="clock" size={16} color="#444" />
+                    <SimpleLineIcons name="puzzle" size={16} color="#444" />
                 </View>
                 <View style={{width: '72%'}}>
-                    <Text style={{fontSize: 12, fontFamily: 'Brandon_Medium', color: '#444'}}>LATEST UPDATE</Text>
+                    <Text style={{fontSize: 12, fontFamily: 'Brandon_Medium', color: '#444'}}>LATEST STUDIED</Text>
                 </View>
                 <View style={{width: '20%', alignItems: 'flex-end'}}>
                     <TouchableOpacity style={{width: '100%', height: 24, justifyContent:'flex-start', alignItems: 'flex-end'}}>
@@ -25,7 +25,7 @@ const History = (props) => {
                 </View>
             </View>
             <View style={{width: '100%', flexDirection: 'row', marginTop: 10}}>
-                <View style={{width: width / 2.8, height: '100%', marginRight: 20}}>
+                <View style={{width: width / 3, height: '100%', marginRight: 20}}>
                     <LinearGradient colors={['#6754b9','#3d2c88']} style={{width: '100%', height: width / 1.9, borderRadius: 6, elevation: 20, padding: 15}}>
                         <SimpleLineIcons name="calculator" size={20} color="#dcd4ff" />
                         <View style={{marginTop: 30}}>
@@ -37,6 +37,34 @@ const History = (props) => {
                             <View style={{width: '50%', height: 4, borderRadius: 2, backgroundColor: '#3d2c88'}}></View>
                         </View>
                         <Text style={{fontSize: 12, fontFamily: 'SourceSansPro-SemiBold', color: '#dcd4ff', marginTop: 5}}>5 of 10 lessons</Text>
+                    </LinearGradient>
+                </View>
+                <View style={{width: width / 3, height: '100%', marginRight: 20}}>
+                    <LinearGradient colors={['#9fb2ac','#505e5e']} style={{width: '100%', height: width / 1.9, borderRadius: 6, elevation: 20, padding: 15}}>
+                        <SimpleLineIcons name="chemistry" size={20} color="#e5fdf5" />
+                        <View style={{marginTop: 30}}>
+                            <Text style={{fontSize: 12, fontFamily: 'SourceSansPro-SemiBold', color: '#e5fdf5'}}>CHEMISTRY</Text>
+                            <Text style={{fontSize: 16, fontFamily: 'PlayfairDisplay-Bold', color: '#fff'}}>Quantum Numbers</Text>
+                            <Text style={{fontSize: 12, fontFamily: 'SourceSansPro-Regular', color: '#e5fdf5'}}>Thu, Nov 20</Text>
+                        </View>
+                        <View style={{width: '100%', height: 8, borderRadius: 4, backgroundColor: '#9fb2ac', marginTop: 15, justifyContent: 'center', paddingHorizontal: 2}}>
+                            <View style={{width: '45%', height: 4, borderRadius: 2, backgroundColor: '#505e5e'}}></View>
+                        </View>
+                        <Text style={{fontSize: 12, fontFamily: 'SourceSansPro-SemiBold', color: '#e5fdf5', marginTop: 5}}>7 of 15 lessons</Text>
+                    </LinearGradient>
+                </View>
+                <View style={{width: width / 3, height: '100%', marginRight: 20}}>
+                    <LinearGradient colors={['#f0a168','#c26c55']} style={{width: '100%', height: width / 1.9, borderRadius: 6, elevation: 20, padding: 15}}>
+                        <SimpleLineIcons name="energy" size={20} color="#ffebdc" />
+                        <View style={{marginTop: 30}}>
+                            <Text style={{fontSize: 12, fontFamily: 'SourceSansPro-SemiBold', color: '#ffebdc'}}>PHYSICS</Text>
+                            <Text style={{fontSize: 16, fontFamily: 'PlayfairDisplay-Bold', color: '#fff'}}>Micrometer Screw</Text>
+                            <Text style={{fontSize: 12, fontFamily: 'SourceSansPro-Regular', color: '#ffebdc'}}>Thu, Nov 20</Text>
+                        </View>
+                        <View style={{width: '100%', height: 8, borderRadius: 4, backgroundColor: '#f0a168', marginTop: 15, justifyContent: 'center', paddingHorizontal: 2}}>
+                            <View style={{width: '70%', height: 4, borderRadius: 2, backgroundColor: '#c26c55'}}></View>
+                        </View>
+                        <Text style={{fontSize: 12, fontFamily: 'SourceSansPro-SemiBold', color: '#ffebdc', marginTop: 5}}>10 of 17 lessons</Text>
                     </LinearGradient>
                 </View>
             </View>
